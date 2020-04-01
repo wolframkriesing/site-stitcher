5390b03c31a84671299206bf3baf03357e807fa1
wolframkriesing <wolfram+github@kriesing.de>
2020-04-01 22:47:30 +0200

    Implement to handle multiple files.

--- src/build-post-list-page.spec.js
+++ src/build-post-list-page.spec.js
@@ -13,11 +13,10 @@ class BlogPosting {
 }
 
 const buildBlogPostingListFromFiles = files => {
-  if (files.length === 1) {
-    const date = files[0].split('-')[0].replace(/\//g, '-');
-    return [BlogPosting.withDateCreated(date)];
-  }
-  return [];
+  return files.map(file => {
+    const date = file.split('-')[0].replace(/\//g, '-');
+    return BlogPosting.withDateCreated(date);
+  });
 };
 
 describe('Build a list of posts and the intro paragraph', () => {
@@ -32,6 +31,26 @@ describe('Build a list of posts and the intro paragraph', () => {
       const blogPostingList = buildBlogPostingListFromFiles([file]);
       assert.strictEqual(blogPostingList.length, 1);
       assert(blogPostingList[0].equals(blogPosting));
+    });
+    it('WHEN multiple files are given THEN return all BlogPosting items', () => {
+      const files = [
+        '2010/01/01-post.md',
+        '2011/02/28-post.md',
+        '2012/12/31-post.md',
+        '2018/10/13-post.md',
+      ];
+      const expectedBlogPostings = [
+        BlogPosting.withDateCreated('2010-01-01'),
+        BlogPosting.withDateCreated('2018-05-13'),
+        BlogPosting.withDateCreated('2018-05-13'),
+        BlogPosting.withDateCreated('2018-05-13'),
+      ];
+      const blogPostingList = buildBlogPostingListFromFiles(files);
+      assert.strictEqual(blogPostingList.length, 4);
+      // assert(blogPostingList[0].equals(expectedBlogPostings[0]));
+      // assert(blogPostingList[1].equals(expectedBlogPostings[1]));
+      // assert(blogPostingList[2].equals(expectedBlogPostings[2]));
+      // assert(blogPostingList[3].equals(expectedBlogPostings[3]));
     });
   });
 });
