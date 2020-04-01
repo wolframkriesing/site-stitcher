928f993ac7f27aa81405562e5615a4cb497f451c
wolframkriesing <wolfram+github@kriesing.de>
2020-04-01 23:53:50 +0200

    Rename "BlogPosting" to "BlogPost" that name was painful, sorry schema.org, but it was so strange.

---
+++ src/blog-post-list.js
@@ -0,0 +1,20 @@
+export class BlogPost {
+  static withDateCreated(dateCreated) {
+    const blogPost = new BlogPost();
+    blogPost.dateCreated = dateCreated;
+    return blogPost;
+  }
+  constructor(attributes = {}) {
+    Object.entries(attributes).forEach(([key, value]) => this[key] = value);
+  }
+  equals(blogPost) {
+    return this.dateCreated === blogPost.dateCreated;
+  }
+}
+
+export const buildBlogPostListFromFiles = files => {
+  return files.map(file => {
+    const date = file.split('-')[0].replace(/\//g, '-');
+    return BlogPost.withDateCreated(date);
+  });
+};
--- src/blog-post-list.spec.js
+++ src/blog-post-list.spec.js
@@ -1,12 +1,12 @@
 import {describe, it} from 'mocha';
 import assert from 'assert';
-import {buildBlogPostingListFromFiles, BlogPosting} from './blog-posting-list.js';
+import {buildBlogPostListFromFiles, BlogPost} from './blog-post-list.js';
 
-const loadBlogPostingList = ({loadBlogPostingFromFile}) => async blogPostingList => {
+const loadBlogPostList = ({loadBlogPostingFromFile}) => async blogPostingList => {
   const rawPosts = await Promise.all(blogPostingList.map(async () => await loadBlogPostingFromFile(/* TODO pass the date */)));
   
   return rawPosts.map((rawPostData, index) => {
-    return new BlogPosting({
+    return new BlogPost({
       dateCreated: blogPostingList[index].dateCreated,
       ...rawPostData
     });
@@ -16,17 +16,17 @@ const loadBlogPostingList = ({loadBlogPostingFromFile}) => async blogPostingList
 describe('Build a list of posts and the intro paragraph', () => {
   describe('GIVEN a list of files', () => {
     it('WHEN empty THEN no posts are returned', () => {
-      const blogPostingList = buildBlogPostingListFromFiles([]);
+      const blogPostingList = buildBlogPostListFromFiles([]);
       assert.deepStrictEqual(blogPostingList, []);
     });
-    it('WHEN one file is given THEN return one BlogPosting', () => {
+    it('WHEN one file is given THEN return one BlogPost', () => {
       const file = '2018/05/13-post.md';
-      const blogPosting = BlogPosting.withDateCreated('2018-05-13');
-      const blogPostingList = buildBlogPostingListFromFiles([file]);
+      const blogPosting = BlogPost.withDateCreated('2018-05-13');
+      const blogPostingList = buildBlogPostListFromFiles([file]);
       assert.strictEqual(blogPostingList.length, 1);
       assert(blogPostingList[0].equals(blogPosting));
     });
-    it('WHEN multiple files are given THEN return all BlogPosting items', () => {
+    it('WHEN multiple files are given THEN return all BlogPost items', () => {
       const files = [
         '2010/01/01-post.md',
         '2011/02/28-post.md',
@@ -34,12 +34,12 @@ describe('Build a list of posts and the intro paragraph', () => {
         '2018/10/13-post.md',
       ];
       const expectedBlogPostings = [
-        BlogPosting.withDateCreated('2010-01-01'),
-        BlogPosting.withDateCreated('2011-02-28'),
-        BlogPosting.withDateCreated('2012-12-31'),
-        BlogPosting.withDateCreated('2018-10-13'),
+        BlogPost.withDateCreated('2010-01-01'),
+        BlogPost.withDateCreated('2011-02-28'),
+        BlogPost.withDateCreated('2012-12-31'),
+        BlogPost.withDateCreated('2018-10-13'),
       ];
-      const blogPostingList = buildBlogPostingListFromFiles(files);
+      const blogPostingList = buildBlogPostListFromFiles(files);
       assert.strictEqual(blogPostingList.length, 4);
       assert(blogPostingList[0].equals(expectedBlogPostings[0]));
       assert(blogPostingList[1].equals(expectedBlogPostings[1]));
@@ -47,36 +47,36 @@ describe('Build a list of posts and the intro paragraph', () => {
       assert(blogPostingList[3].equals(expectedBlogPostings[3]));
     });
   });
-  describe('GIVEN load the blog posting preview data', () => {
-    it('WHEN one file is given THEN return one BlogPosting', async () => {
-      const blogPostingList = [BlogPosting.withDateCreated('2018-05-13')];
+  describe('GIVEN load the blog post preview data', () => {
+    it('WHEN one file is given THEN return one BlogPost', async () => {
+      const blogPostingList = [BlogPost.withDateCreated('2018-05-13')];
       const rawBlogPosting = {
         headline: 'This is the first post',
         abstract: 'the first paragraph of the blog post ...'
       };
       const loadBlogPostingFromFile = async () => rawBlogPosting;
-      const completeBlogPostingList = await loadBlogPostingList({loadBlogPostingFromFile})(blogPostingList);
+      const completeBlogPostingList = await loadBlogPostList({loadBlogPostingFromFile})(blogPostingList);
 
       assert.strictEqual(completeBlogPostingList.length, 1);
       const post = completeBlogPostingList[0];
-      const expectedPost = new BlogPosting({
+      const expectedPost = new BlogPost({
         dateCreated: '2018-05-13',
         ...rawBlogPosting
       });
       assert(post.equals(expectedPost));
     });
-    it('WHEN many files are given THEN return all the BlogPosting items', async () => {
+    it('WHEN many files are given THEN return all the BlogPost items', async () => {
       const blogPostingList = [
-        BlogPosting.withDateCreated('2018-05-13'),
-        BlogPosting.withDateCreated('2011-11-11'),
+        BlogPost.withDateCreated('2018-05-13'),
+        BlogPost.withDateCreated('2011-11-11'),
       ];
       const rawBlogPosting = {headline: 'headline', abstract: 'abstract'};
       const loadBlogPostingFromFile = async () => rawBlogPosting;
-      const completeBlogPostingList = await loadBlogPostingList({loadBlogPostingFromFile})(blogPostingList);
+      const completeBlogPostingList = await loadBlogPostList({loadBlogPostingFromFile})(blogPostingList);
 
       const expectedPosts = [
-        new BlogPosting({dateCreated: '2018-05-13', ...rawBlogPosting}),
-        new BlogPosting({dateCreated: '2011-11-11', ...rawBlogPosting}),
+        new BlogPost({dateCreated: '2018-05-13', ...rawBlogPosting}),
+        new BlogPost({dateCreated: '2011-11-11', ...rawBlogPosting}),
       ];
       assert.strictEqual(completeBlogPostingList.length, 2);
       assert(completeBlogPostingList[0].equals(expectedPosts[0]));
