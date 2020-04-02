a00ca88d9123bffe15d04eaa15b386da8dc76d12
wolframkriesing <wolfram+github@kriesing.de>
2020-04-03 01:52:32 +0200

    Use tundra to render the posts dynamically.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import * as path from 'path';
+import Tundra from 'tundrajs';
+const tundra = new Tundra();
+
+import {preloadBlogPostListFromDirectory} from './preload-blog-post.js';
+import {loadBlogPostList} from './load-blog-post.js';
+
+(async() => {
+  const postsDirectory = path.join(__dirname, '../content/blog-posts');
+  const posts = await loadBlogPostList()(await preloadBlogPostListFromDirectory()(postsDirectory));
+
+  tundra.setBase(path.join(__dirname, 'templates'));
+  const v = tundra.getRender('index.html', {posts});
+  console.log(v);
+})();
+
