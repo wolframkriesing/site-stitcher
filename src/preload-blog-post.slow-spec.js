0e3566f7b385e7098c7442b8b37f703779881d54
wolframkriesing <wolfram+github@kriesing.de>
2020-04-05 23:13:52 +0200

    Create test-content to run tests against.

--- src/preload-blog-post.slow-spec.js
+++ src/preload-blog-post.slow-spec.js
@@ -3,13 +3,13 @@ import assert from 'assert';
 import * as path from 'path';
 import {preloadBlogPostListFromDirectory} from './preload-blog-post.js';
 
-const blogPostsDirectory = path.join(__dirname, '../content/blog-posts');
+const blogPostsDirectory = path.join(__dirname, '../test-content/blog-posts');
 
 describe('Preload blog posts from a given directory (tests are slow, working against a real fs)', () => {
   it('GIVEN directory with blog posts WHEN preloading THEN return blog posts with `dateCreated` set', async () => {
     const posts = await preloadBlogPostListFromDirectory()(blogPostsDirectory);
     assert(posts.length > 0);
-    assert.strictEqual(posts[0].dateCreated, '2017-11-03');
-    assert.strictEqual(posts[0].filename, `${blogPostsDirectory}/2017/11/03-mikado-method-to-install-a-vim-plugin.md`);
+    assert.strictEqual(posts[0].dateCreated, '2000-01-01');
+    assert.strictEqual(posts[0].filename, `${blogPostsDirectory}/2000/01/01-normal-post.md`);
   });
 });
---
+++ test-content/blog-posts/2000/01/01-normal-post.md
@@ -0,0 +1,5 @@
+# Normal Post
+
+Abstract
+
+And the body ...
