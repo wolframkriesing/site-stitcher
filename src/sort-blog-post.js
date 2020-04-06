ee31bbdb1f39eef9e73cad1822fd9f7b6b5ca9eb
wolframkriesing <wolfram+github@kriesing.de>
2020-04-06 21:48:14 +0200

    Sort descending and use it on the overview.

--- src/sort-blog-post.js
+++ src/sort-blog-post.js
@@ -1,1 +1,2 @@
-export const sortByDateCreatedAsc = (p1, p2) => p1.dateCreated < p2.dateCreated ? -1 : 1;
+export const sortByDateCreatedAscending = (p1, p2) => p1.dateCreated < p2.dateCreated ? -1 : 1;
+export const sortByDateCreatedDescending = (p1, p2) => p1.dateCreated > p2.dateCreated ? -1 : 1;
--- src/sort-blog-post.spec.js
+++ src/sort-blog-post.spec.js
@@ -1,23 +1,33 @@
 import {describe, it} from 'mocha';
 import assert from 'assert';
 import {BlogPost} from './BlogPost.js';
-import {sortByDateCreatedAsc} from './sort-blog-post.js';
+import {sortByDateCreatedAscending, sortByDateCreatedDescending} from './sort-blog-post.js';
 
 describe('Sort blog posts by date', () => {
-  it('GIVEN many unsorted posts WHEN sorted THEN they are in the right order', () => {
-    const unsortedPosts = [
-      BlogPost.withDateCreated('2002-03-03'),
-      BlogPost.withDateCreated('2002-02-02'),
-      BlogPost.withDateCreated('2004-04-04'),
-      BlogPost.withDateCreated('2001-01-01'),
-    ];
+  const unsortedPosts = [
+    BlogPost.withDateCreated('2002-03-03'),
+    BlogPost.withDateCreated('2002-02-02'),
+    BlogPost.withDateCreated('2004-04-04'),
+    BlogPost.withDateCreated('2001-01-01'),
+  ];
+  it('GIVEN many unsorted posts WHEN sorted ascending THEN they are in the right order', () => {
     const expectedPosts = [
       BlogPost.withDateCreated('2001-01-01'),
       BlogPost.withDateCreated('2002-02-02'),
       BlogPost.withDateCreated('2002-03-03'),
       BlogPost.withDateCreated('2004-04-04'),
     ];
-    const sorted = unsortedPosts.sort(sortByDateCreatedAsc);
+    const sorted = unsortedPosts.sort(sortByDateCreatedAscending);
+    assert.deepStrictEqual(sorted, expectedPosts);
+  });
+  it('GIVEN many unsorted posts WHEN sorted descending THEN they are in the right order', () => {
+    const expectedPosts = [
+      BlogPost.withDateCreated('2004-04-04'),
+      BlogPost.withDateCreated('2002-03-03'),
+      BlogPost.withDateCreated('2002-02-02'),
+      BlogPost.withDateCreated('2001-01-01'),
+    ];
+    const sorted = unsortedPosts.sort(sortByDateCreatedDescending);
     assert.deepStrictEqual(sorted, expectedPosts);
   });
 });
