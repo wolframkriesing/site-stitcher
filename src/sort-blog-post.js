8c1b0e0aea692e07944cfa7eeb3bdc76318b654a
wolframkriesing <wolfram+github@kriesing.de>
2020-04-06 21:43:35 +0200

    Extract the date sorting fn.

--- src/sort-blog-post.js
+++ src/sort-blog-post.js
@@ -1,2 +1,4 @@
+const sortByDateCreatedAsc = (p1, p2) => p1.dateCreated < p2.dateCreated ? -1 : 1;
+
 export const sortBlogPostListByDateCreated = blogPostList =>
-  blogPostList.sort((p1, p2) => p1.dateCreated < p2.dateCreated ? -1 : 1);
+  blogPostList.sort(sortByDateCreatedAsc);
