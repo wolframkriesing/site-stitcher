801149af06fa83a8070f4861f9e73cc1e3e48ebf
wolframkriesing <wolfram+github@kriesing.de>
2020-04-06 21:43:23 +0200

    Sort the blog posts ... but actually we want it the other way around.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ const tundra = new Tundra();
 
 import {preloadBlogPostListFromDirectory} from './preload-blog-post.js';
 import {loadBlogPostList} from './load-blog-post.js';
+import {sortBlogPostListByDateCreated} from './sort-blog-post.js';
 
 (async() => {
   const postsDirectory = path.join(__dirname, '../content/blog-posts');
-  const posts = await loadBlogPostList()(await preloadBlogPostListFromDirectory()(postsDirectory));
+  const posts = sortBlogPostListByDateCreated(
+    await loadBlogPostList()(await preloadBlogPostListFromDirectory()(postsDirectory))
+  );
 
   tundra.setBase(path.join(__dirname, 'templates'));
   const v = tundra.getRender('index.html', {posts});
