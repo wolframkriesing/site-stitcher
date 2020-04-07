aef88aa05581024123957fde6bcb4da747e15759
wolframkriesing <wolfram+github@kriesing.de>
2020-04-07 23:28:57 +0200

    Fix the test to test the right property.

--- src/preload-blog-post.slow-spec.js
+++ src/preload-blog-post.slow-spec.js
@@ -15,6 +15,7 @@ describe('Preload blog posts from a given directory (tests are slow, working aga
   });
   it('GIVEN a file that does NOT start with a number (the day) THEN dont find it as blog post', async () => {
     const posts = await preloadBlogPostListFromDirectory()(blogPostsDirectory);
-    assertThat(posts, not(hasItem(hasProperty('filename', endsWith('2000/01/not-a-blog-post.txt')))));
+    assertThat(posts[0], not(hasItem(hasProperty('markdownFilename', endsWith('2000/01/not-a-blog-post.txt')))));
+    // how can we write this test better, it tests something that is not there ... kinda stupid :)
   });
 });
