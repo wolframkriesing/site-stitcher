a6e144872bbdfe71b0730f92068002a32eb2df1f
wolframkriesing <wolfram+github@kriesing.de>
2020-04-09 00:35:33 +0200

    Hacked out generation of all posts.

--- src/index.js
+++ src/index.js
@@ -8,14 +8,31 @@ import {sortByDateCreatedDescending} from './sort-blog-post.js';
 
 import {toReadableDate, toWeekday} from './date.js';
 
+tundra.setBase(path.join(__dirname, 'templates'));
+
+import * as fs from 'fs';
+import marked from 'marked';
+const generatePost = (post) => {
+  const destDir = path.join(__dirname, '../_output', post.url);
+  fs.mkdirSync(destDir, {recursive: true});
+  const destFilename = path.join(destDir, 'index.html');
+  const postHtml = marked(fs.readFileSync(post.markdownFilename, 'utf8'));
+  const renderedFile = tundra.getRender('post.html', {post, postHtml});
+  fs.writeFileSync(destFilename, renderedFile);
+console.log("Built ", destFilename);
+}
+
 (async() => {
   const postsDirectory = path.join(__dirname, '../content/blog-posts');
   const posts = (
     await loadBlogPostList()(await preloadBlogPostListFromDirectory()(postsDirectory))
   ).sort(sortByDateCreatedDescending);
 
-  tundra.setBase(path.join(__dirname, 'templates'));
-  const v = tundra.getRender('index.html', {posts, toReadableDate, toWeekday});
-  console.log(v);
+  posts.forEach(generatePost);
+
+  const renderedFile = tundra.getRender('index.html', {posts, toReadableDate, toWeekday});
+  const destFilename = path.join(__dirname, '../_output', 'index.html');
+  fs.writeFileSync(destFilename, renderedFile);
+console.log("Built ", destFilename);
 })();
 
