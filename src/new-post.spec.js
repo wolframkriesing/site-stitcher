6eadacc3b9d740e7d4aeb5fd16b5d2a781fbf166
wolframkriesing <wolfram+github@kriesing.de>
2020-04-10 12:55:32 +0200

    Start implementing the script which collects the user's input in real time for creating a new blog post.

---
+++ src/new-post.spec.js
@@ -0,0 +1,37 @@
+import {describe, it} from 'mocha';
+import assert from 'assert';
+import {assertThat, equalTo, hasProperties} from 'hamjest';
+
+const askForHeadline = async (askUser) => {
+  const headline = await askUser('Headline: ');
+  if (headline === '') return await askUser('Headline: ');
+  return headline;
+}
+
+const collectBlogPostData = ({askUser}) => async () => {
+  askForHeadline(askUser);
+};
+
+describe('Script for creating a new blog post skeleton', () => {
+  it('WHEN asking user for the headline THEN ask for "Headline: "', async () => {
+    let questionAsked = '';
+    const askUser = async question => { questionAsked = question; return 'a headline'; };
+    await collectBlogPostData({askUser})();
+    assert.strictEqual(questionAsked, 'Headline: ');
+  });
+  it('WHEN user enters no headline THEN ask again', async () => {
+    const questionsAsked = [];
+    const answers = ['', 'a headline'];
+    const askUser = async question => { questionsAsked.push(question); return answers.shift(); };
+    await collectBlogPostData({askUser})();
+    assert.deepStrictEqual(questionsAsked, ['Headline: ', 'Headline: ']);
+  });
+  xit('WHEN title is given THEN set it as `headline`', () => {
+    const askUser = async question => 'user-entered title';
+    const postData = collectBlogPostData({askUser})();
+    assertThat(postData, {headline: 'user-entered title'});
+  });
+  it('if asking for a title and user enters nothing, ask again', () => {
+    
+  });
+});
