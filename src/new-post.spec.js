bdf0e09b46f618bda6688b6102ad916d0f32cb41
wolframkriesing <wolfram+github@kriesing.de>
2020-04-10 13:25:56 +0200

    Ask user for abstract. Made me realize that other tests needed change ... makes me think.

--- src/new-post.spec.js
+++ src/new-post.spec.js
@@ -2,40 +2,50 @@ import {describe, it} from 'mocha';
 import assert from 'assert';
 import {assertThat, equalTo, hasProperties} from 'hamjest';
 
-const askForHeadline = async (askUser) => {
+const askForRequiredField = (askUser) => async (text) => {
   let headline = '';
   while (headline === '') {
-    headline = await askUser('Headline: ');
+    headline = await askUser(`${text}: `);
   }
   return headline;
 }
 
 const collectBlogPostData = ({askUser}) => async () => {
   const post = {};
-  post.headline = await askForHeadline(askUser);
+  post.headline = await askForRequiredField(askUser)('Headline');
+  post.abstract = await askForRequiredField(askUser)('Abstract');
   return post;
 };
 
 describe('Script for creating a new blog post skeleton', () => {
   it('WHEN asking user for the headline THEN ask for "Headline: "', async () => {
-    let questionAsked = '';
-    const askUser = async question => { questionAsked = question; return 'a headline'; };
+    const questionsAsked = [];
+    const askUser = async question => { questionsAsked.push(question); return 'a headline'; };
     await collectBlogPostData({askUser})();
-    assert.strictEqual(questionAsked, 'Headline: ');
+    assert.strictEqual(questionsAsked[0], 'Headline: ');
   });
   it('WHEN user enters no headline THEN ask again until there is one', async () => {
     const questionsAsked = [];
     const answers = ['', '', '', '', 'a headline'];
     const askUser = async question => { questionsAsked.push(question); return answers.shift(); };
     await collectBlogPostData({askUser})();
-    assert.deepStrictEqual(questionsAsked, ['Headline: ', 'Headline: ', 'Headline: ', 'Headline: ', 'Headline: ']);
+    assert.deepStrictEqual(questionsAsked.slice(0, 5), ['Headline: ', 'Headline: ', 'Headline: ', 'Headline: ', 'Headline: ']);
   });
-  it('WHEN headline is given THEN set it as `headline`', async () => {
+  it('WHEN headline is given THEN return it as `headline`', async () => {
     const askUser = async q => 'user-entered title';
     const postData = await collectBlogPostData({askUser})();
-    assertThat(postData, {headline: 'user-entered title'});
+    assertThat(postData, hasProperties({headline: 'user-entered title'}));
   });
-  it('if asking for a title and user enters nothing, ask again', () => {
-    
+  it('WHEN asking for an abstract THEN ask for "Abstract: " until there is one', async () => {
+    const questionsAsked = [];
+    const answers = ['a headline', '', 'an abstract'];
+    const askUser = async question => { questionsAsked.push(question); return answers.shift(); };
+    await collectBlogPostData({askUser})();
+    assert.deepStrictEqual(questionsAsked, ['Headline: ', 'Abstract: ', 'Abstract: ']);
+  });
+  it('WHEN abstract is given THEN return it as `abstract`', async () => {
+    const askUser = async q => 'an abstract';
+    const postData = await collectBlogPostData({askUser})();
+    assertThat(postData, hasProperties({abstract: 'an abstract'}));
   });
 });
