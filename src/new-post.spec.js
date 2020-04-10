3ef3d813b3e65cf654065b68e28df865625b27f3
wolframkriesing <wolfram+github@kriesing.de>
2020-04-10 14:19:43 +0200

    Start enriching the data.

--- src/new-post.spec.js
+++ src/new-post.spec.js
@@ -14,7 +14,12 @@ const askForRequiredField = (askUser) => async (text) => {
   return headline;
 }
 
-const collectBlogPostData = ({askUser}) => async () => {
+/**
+ * @typedef {{headline: string, abstract: string, tags: string[]}} RawBlogPostData
+ * @param askUser
+ * @returns {function(): function(): Promise<RawBlogPostData>}
+ */
+const collectBlogPostDataFromUser = ({askUser}) => async () => {
   const post = {};
   post.headline = await askForRequiredField(askUser)('Headline');
   post.abstract = await askForRequiredField(askUser)('Abstract');
@@ -22,39 +27,58 @@ const collectBlogPostData = ({askUser}) => async () => {
   return post;
 };
 
+/**
+ * @param {RawBlogPostData} rawPostData
+ * @return {function(): BlogPost}
+ */
+const enrichNewPostData = ({nowAsDateTimeString}) => (rawPostData) => {
+  return {dateCreated: nowAsDateTimeString()};
+}
+
 describe('Script for creating a new blog post skeleton', () => {
-  describe('WHEN asking the user to enter the data', () => {
-    it('THEN ask for headline, abstract and tags', async () => {
-      const questionsAsked = [];
-      const answers = ['a headline', 'an abstract', 'tags'];
-      const askUser = async question => { questionsAsked.push(question); return answers.shift(); };
-      await collectBlogPostData({askUser})();
-      assert.deepStrictEqual(questionsAsked, ['Headline: ', 'Abstract: ', 'Tags: ']);
+  describe('GIVEN the user is asked for the data for a new post', () => {
+    describe('WHEN asking the user to enter the data', () => {
+      it('THEN ask for headline, abstract and tags', async () => {
+        const questionsAsked = [];
+        const answers = ['a headline', 'an abstract', 'tags'];
+        const askUser = async question => { questionsAsked.push(question); return answers.shift(); };
+        await collectBlogPostDataFromUser({askUser})();
+        assert.deepStrictEqual(questionsAsked, ['Headline: ', 'Abstract: ', 'Tags: ']);
+      });
+      it('AND any is missing THEN ask until each is entered', async () => {
+        const questionsAsked = [];
+        const noAnswer = '';
+        const answers = [noAnswer, 'a headline', noAnswer, noAnswer, 'an abstract', noAnswer, 'tags'];
+        const askUser = async question => { questionsAsked.push(question); return answers.shift(); };
+        await collectBlogPostDataFromUser({askUser})();
+        assert.deepStrictEqual(questionsAsked, [
+          'Headline: ', 'Headline: ',
+          'Abstract: ', 'Abstract: ', 'Abstract: ',
+          'Tags: ', 'Tags: ',
+        ]);
+      });
     });
-    it('AND any is missing THEN ask until each is entered', async () => {
-      const questionsAsked = [];
-      const noAnswer = '';
-      const answers = [noAnswer, 'a headline', noAnswer, noAnswer, 'an abstract', noAnswer, 'tags'];
-      const askUser = async question => { questionsAsked.push(question); return answers.shift(); };
-      await collectBlogPostData({askUser})();
-      assert.deepStrictEqual(questionsAsked, [
-        'Headline: ', 'Headline: ',
-        'Abstract: ', 'Abstract: ', 'Abstract: ',
-        'Tags: ', 'Tags: ',
-      ]);
+    describe('WHEN data are entered', async () => {
+      const postData = async () => {
+        const answers = ['headline', 'abstract', 'tags'];
+        const askUser = async q => answers.shift();
+        return await collectBlogPostDataFromUser({askUser})();
+      };
+      it('WHEN data are entered THEN return them in the according property', async () => {
+        assertThat(await postData(), hasProperties({headline: 'headline', abstract: 'abstract'}));
+      });
+      it('THEN tags are returns as array', async () => {
+        assertThat(await postData(), hasProperties({tags: ['tags']}));
+      });
     });
   });
-  describe('WHEN data are entered', async () => {
-    const postData = async () => {
-      const answers = ['headline', 'abstract', 'tags'];
-      const askUser = async q => answers.shift();
-      return await collectBlogPostData({askUser})();
-    };
-    it('WHEN data are entered THEN return them in the according property', async () => {
-      assertThat(await postData(), hasProperties({headline: 'headline', abstract: 'abstract'}));
-    });
-    it('THEN tags are returns as array', async () => {
-      assertThat(await postData(), hasProperties({tags: ['tags']}));
+  describe('GIVEN user-entered data for a new post make a proper `BlogPost` out of them', () => {
+    it('WHEN enriching these data THEN add the current date', () => {
+      const dateCreated = '2001-01-01 11:11 CET';
+      const nowAsDateTimeString = () => dateCreated;
+      const rawPostData = {headline: '', abstract: '', tags: []};
+      const enriched = enrichNewPostData({nowAsDateTimeString})(rawPostData);
+      assertThat(enriched, hasProperties({dateCreated}));
     });
   });
 });
