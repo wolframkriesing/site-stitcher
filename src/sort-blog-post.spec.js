9f113dd66e411aa7bfa656de37fde78619c63aac
wolframkriesing <wolfram+github@kriesing.de>
2020-04-11 22:41:25 +0200

    Simplify sort functions, no need for using BlogPost.

--- src/sort-blog-post.spec.js
+++ src/sort-blog-post.spec.js
@@ -1,31 +1,30 @@
 import {describe, it} from 'mocha';
 import assert from 'assert';
-import {BlogPost} from './BlogPost.js';
 import {sortByDateCreatedAscending, sortByDateCreatedDescending} from './sort-blog-post.js';
 
-describe('Sort blog posts by date', () => {
+describe('Sort objects (or blog posts) by property `dateCreated`', () => {
   const unsortedPosts = [
-    BlogPost.preload('2002/03/03-post.md'),
-    BlogPost.preload('2002/02/02-post.md'),
-    BlogPost.preload('2004/04/04-post.md'),
-    BlogPost.preload('2001/01/01-post.md'),
+    {dateCreated: '2002-03-03'},
+    {dateCreated: '2002-02-02'},
+    {dateCreated: '2004-04-04'},
+    {dateCreated: '2001-01-01'},
   ];
   it('GIVEN many unsorted posts WHEN sorted ascending THEN they are in the right order', () => {
     const expectedPosts = [
-      BlogPost.preload('2001/01/01-post.md'),
-      BlogPost.preload('2002/02/02-post.md'),
-      BlogPost.preload('2002/03/03-post.md'),
-      BlogPost.preload('2004/04/04-post.md'),
+      {dateCreated: '2001-01-01'},
+      {dateCreated: '2002-02-02'},
+      {dateCreated: '2002-03-03'},
+      {dateCreated: '2004-04-04'},
     ];
     const sorted = unsortedPosts.sort(sortByDateCreatedAscending);
     assert.deepStrictEqual(sorted, expectedPosts);
   });
   it('GIVEN many unsorted posts WHEN sorted descending THEN they are in the right order', () => {
     const expectedPosts = [
-      BlogPost.preload('2004/04/04-post.md'),
-      BlogPost.preload('2002/03/03-post.md'),
-      BlogPost.preload('2002/02/02-post.md'),
-      BlogPost.preload('2001/01/01-post.md'),
+      {dateCreated: '2004-04-04'},
+      {dateCreated: '2002-03-03'},
+      {dateCreated: '2002-02-02'},
+      {dateCreated: '2001-01-01'},
     ];
     const sorted = unsortedPosts.sort(sortByDateCreatedDescending);
     assert.deepStrictEqual(sorted, expectedPosts);
