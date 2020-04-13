8595fa4a8b30350af8576f6d897983bbf89c3362
wolframkriesing <wolfram+github@kriesing.de>
2020-04-13 16:33:30 +0200

    Make it work on mobile including a hamburger-kinda navigation.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import * as path from 'path';
 import marked from 'marked';
 import Tundra from 'tundrajs';
+import * as fs from 'fs';
+
 const tundra = new Tundra();
 
 import {loadManyBlogPostSourceFiles} from './load-blog-post-source-file.js';
@@ -10,14 +12,25 @@ import {sortByDateCreatedDescending} from './sort-blog-post.js';
 import {toReadableDate, toWeekday} from './date.js';
 const toReadableDateTime = s => s;
 
+
+const navigationItems = [
+  {path: '/', name: 'Home'},
+  {path: '/about', name: 'About'},
+];
+const defaultRenderParams = {
+  navigationItems,
+  toReadableDateTime,
+  toReadableDate,
+  toWeekday,
+};
+
 tundra.setBase(path.join(__dirname, 'templates'));
 
-import * as fs from 'fs';
 const generatePost = async (post) => {
   const destDir = path.join(__dirname, '../_output', post.url);
   await fs.promises.mkdir(destDir, {recursive: true});
   const destFilename = path.join(destDir, 'index.html');
-  const renderedFile = tundra.getRender('post.html', {post, toReadableDateTime});
+  const renderedFile = tundra.getRender('post.html', {...defaultRenderParams, post});
   await fs.promises.writeFile(destFilename, renderedFile);
   console.log("Built ", destFilename);
 }
@@ -28,13 +41,13 @@ const generateAboutPage = async () => {
   await fs.promises.mkdir(destDir, {recursive: true});
   const destFilename = path.join(destDir, 'index.html');
   const content = marked(await fs.promises.readFile(path.join(contentDir, 'about.md'), 'utf8'));
-  const renderedFile = tundra.getRender('about.html', {content});
+  const renderedFile = tundra.getRender('about.html', {...defaultRenderParams, content});
   await fs.promises.writeFile(destFilename, renderedFile);
   console.log("Built ", destFilename);
 }
 
 const generateHomePage = async (posts) => {
-  const renderedFile = tundra.getRender('index.html', {posts, toReadableDate, toReadableDateTime, toWeekday});
+  const renderedFile = tundra.getRender('index.html', {...defaultRenderParams, posts});
   const destFilename = path.join(__dirname, '../_output', 'index.html');
   await fs.promises.writeFile(destFilename, renderedFile);
   console.log("Built ", destFilename);
