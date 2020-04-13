88155f6cc55deff5ba567c7d06aaf0e16da8816c
wolframkriesing <wolfram+github@kriesing.de>
2020-04-13 12:34:35 +0200

    Move fs functionality out of the load file.

--- src/load-blog-post-source-file.js
+++ src/load-blog-post-source-file.js
@@ -1,51 +1,6 @@
-import * as fs from 'fs';
 import * as path from 'path';
+import {findFilesInDir} from './_deps/fs.js';
 import {BlogPostSourceFile} from './BlogPostSourceFile.js';
-
-const toAbsoluteDirectoryName = (dir) => (entries) => {
-  return entries
-    .filter(f => f.isDirectory())
-    .map(entry => path.join(dir, entry.name))
-};
-const toAbsoluteDirectoryOrFilename = (dir) => (entries) => {
-  return entries
-    .map(entry => path.join(dir, entry.name))
-};
-const filenameStartRegex = /\d\d-/;
-const findOnlyBlogPostSourceFilesOrDirs = (entries) => {
-  return entries
-    .filter(entry => entry.name.match(filenameStartRegex))
-  ;
-};
-/**
- * TODO not sure that this function should also filter the files and folders by name, this might rather be a "biz log" not a dependency logic
- * // @typedef {BlogPostFilename} = 2000/01/01-xxxx.md
- * @param dir
- * @returns {Promise<BlogPostFilename>}
- */
-const findFilesInDir = async (dir) => {
-  const findYearDirectories = async (dir) => {
-    return await fs.promises.readdir(dir, {withFileTypes: true})
-      .then(toAbsoluteDirectoryName(dir));
-  };
-  const findMonthDirectories = async (dir) => {
-    return fs.promises.readdir(dir, {withFileTypes: true})
-      .then(toAbsoluteDirectoryName(dir));
-  };
-  const findBlogPostSourceFiles = async (dir) => {
-    return fs.promises.readdir(dir, {withFileTypes: true})
-      .then(findOnlyBlogPostSourceFilesOrDirs)
-      .then(toAbsoluteDirectoryOrFilename(dir))
-    ;
-  };
-  const removeRootBlogPostDirectory = (dir) => (file) =>
-    file.replace(`${dir}/`, '');
-
-  const yearDirectories = await findYearDirectories(dir);
-  const monthDirectories = (await Promise.all(yearDirectories.map(findMonthDirectories))).flat();
-  const files = (await Promise.all(monthDirectories.map(findBlogPostSourceFiles))).flat();
-  return files.map(removeRootBlogPostDirectory(dir));
-};
 
 const prodDeps = () => {
   return {findFilesInDir};
