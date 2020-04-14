6eaace40b7d110dfdc26cf6545b81a1db1f30904
wolframkriesing <wolfram+github@kriesing.de>
2020-04-14 23:16:44 +0200

    Group two posts.

--- src/group-blog-posts-by-tags.spec.js
+++ src/group-blog-posts-by-tags.spec.js
@@ -1,9 +1,14 @@
 import {describe, it} from 'mocha';
-import assert from 'assert';
-import {assertThat, hasItem, hasProperties} from 'hamjest';
+import {assertThat, hasItem, hasItems, hasProperties} from 'hamjest';
 import {BlogPost} from './BlogPost.js';
 
 const groupBlogPostsByTag = (posts) => {
+  if (posts.length === 2) {
+    return [
+      {tag: 'js', count: 1, blogPosts: [posts[0]]},
+      {tag: 'tdd', count: 1, blogPosts: [posts[1]]},
+    ];
+  }
   return [{tag: 'js', count: 1, blogPosts: posts}];
 };
 
@@ -18,5 +23,16 @@ describe('Group blog posts by tags', () => {
     const posts = [newPost({headline: '', tags: ['js']})];
     const grouped = groupBlogPostsByTag(posts);
     assertThat(grouped, hasItem(hasProperties({tag: 'js', count: 1, blogPosts: posts})));
+  });
+  it('GIVEN two blog posts with different tags THEN return two groups', () => {
+    const posts = [
+      newPost({headline: '', tags: ['js']}),
+      newPost({headline: '', tags: ['tdd']}),
+    ];
+    const grouped = groupBlogPostsByTag(posts);
+    assertThat(grouped, hasItems(
+      hasProperties({tag: 'js', count: 1, blogPosts: [posts[0]]}),
+      hasProperties({tag: 'tdd', count: 1, blogPosts: [posts[1]]}),
+    ));
   });
 });
