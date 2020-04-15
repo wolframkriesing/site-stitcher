5ba1713c45a54bdad92139c7bc36f7b121150760
wolframkriesing <wolfram+github@kriesing.de>
2020-04-16 00:42:07 +0200

    One group per tag only.

--- src/blog-posts-by-tags.js
+++ src/blog-posts-by-tags.js
@@ -1,6 +1,7 @@
 const postsByTag = (posts, tag) => posts.filter(post => post.tags.includes(tag));
 const blogPostsGroupedByTag = (tag, posts) => ({tag, blogPosts: postsByTag(posts, tag)});
+const uniques = arr => [...new Set(arr)];
 export const groupBlogPostsByTag = (posts) => {
-  const allTags = posts.map(post => post.tags).flat();
+  const allTags = uniques(posts.map(post => post.tags).flat());
   return allTags.map(tag => blogPostsGroupedByTag(tag, posts));
 };
--- src/blog-posts-by-tags.spec.js
+++ src/blog-posts-by-tags.spec.js
@@ -1,5 +1,5 @@
 import {describe, it} from 'mocha';
-import {assertThat, hasItem, hasItems, hasProperties} from 'hamjest';
+import {assertThat, hasItem, hasItems, hasProperties, contains} from 'hamjest';
 import {BlogPost} from './BlogPost.js';
 import {groupBlogPostsByTag} from './blog-posts-by-tags.js';
 
@@ -26,13 +26,13 @@ describe('Group blog posts by tags', () => {
       hasProperties({tag: 'tdd', blogPosts: [posts[1]]}),
     ));
   });
-  it('GIVEN two blog posts with the same tags each THEN return two groups with each post in it', () => {
+  it('GIVEN two blog posts with the same tags each THEN return exactly two groups with each post in it', () => {
     const posts = [
       newPost({headline: '', tags: ['js', 'tdd']}),
       newPost({headline: '', tags: ['js', 'tdd']}),
     ];
     const grouped = groupBlogPostsByTag(posts);
-    assertThat(grouped, hasItems(
+    assertThat(grouped, contains(
       hasProperties({tag: 'js', blogPosts: posts}),
       hasProperties({tag: 'tdd', blogPosts: posts}),
     ));
