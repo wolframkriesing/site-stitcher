a6ba00c79d03da63111eecbc9341a9f746d8b350
wolframkriesing <wolfram+github@kriesing.de>
2020-04-20 23:30:49 +0200

    Clean up the log output and make it a bit more readable.

--- src/index.js
+++ src/index.js
@@ -44,7 +44,6 @@ const generate404Page = async (posts) => {
   const destFilename = path.join(destDir, '404.html');
   const renderedFile = tundra.getRender('404.html', {...defaultRenderParams, posts});
   await fs.promises.writeFile(destFilename, renderedFile);
-  // console.log("Built 404 page ", destFilename);
 }
 
 const generatePost = async (post) => {
@@ -53,7 +52,6 @@ const generatePost = async (post) => {
   const destFilename = path.join(destDir, 'index.html');
   const renderedFile = tundra.getRender('post.html', {...defaultRenderParams, post});
   await fs.promises.writeFile(destFilename, renderedFile);
-  // console.log("Built ", destFilename);
 }
 
 const generateAboutPage = async () => {
@@ -64,14 +62,12 @@ const generateAboutPage = async () => {
   const content = marked(await fs.promises.readFile(path.join(contentDir, 'about.md'), 'utf8'));
   const renderedFile = tundra.getRender('about.html', {...defaultRenderParams, content});
   await fs.promises.writeFile(destFilename, renderedFile);
-  // console.log("Built ", destFilename);
 }
 
 const generateHomePage = async (posts) => {
   const renderedFile = tundra.getRender('index.html', {...defaultRenderParams, posts});
   const destFilename = path.join(__dirname, '../_output', 'index.html');
   await fs.promises.writeFile(destFilename, renderedFile);
-  // console.log("Built ", destFilename);
 };
 
 const generateTagPage = async (group) => {
@@ -81,7 +77,6 @@ const generateTagPage = async (group) => {
   const destFilename = path.join(destDir, 'index.html');
   const renderedFile = tundra.getRender('tag.html', {...defaultRenderParams, tag, posts: group.blogPosts});
   await fs.promises.writeFile(destFilename, renderedFile);
-  // console.log("Built ", destFilename);
 };
 
 const generateMonthPage = async (group) => {
@@ -91,7 +86,6 @@ const generateMonthPage = async (group) => {
   const destFilename = path.join(destDir, 'index.html');
   const renderedFile = tundra.getRender('month.html', {...defaultRenderParams, yearAndMonth, posts: group.blogPosts});
   await fs.promises.writeFile(destFilename, renderedFile);
-  // console.log("Built ", destFilename);
 };
 
 const generateTagPages = async (postGroups) => Promise.all(postGroups.map(generateTagPage));
@@ -105,6 +99,8 @@ const timeIt = async (label, fn) => {
 
 import {findRelatedPosts} from './related-posts.js';
 (async() => {
+  console.time('Overall');
+  console.log('Preparing data\n========');
   console.time('Load source files');
   const postsDirectory = path.join(__dirname, '../content/blog-posts');
   const sourceFiles = await loadManyBlogPostSourceFiles()(postsDirectory);
@@ -118,29 +114,18 @@ import {findRelatedPosts} from './related-posts.js';
     byTag: groupBlogPostsByTag(posts),
     byMonth: groupBlogPostsByYearAndMonth(posts),
   };
-console.log("posts= ", posts.length);
-console.log("groupedBlogPosts= ", groupedBlogPosts.byTag.length);
   defaultRenderParams.groupedBlogPosts = groupedBlogPosts;
   console.timeEnd('Relate and group posts');
-  // Promise.all([
-  //   timeIt('All posts', () => Promise.all(posts.map(generatePost))),
-  //   timeIt('All 301 pages', () => Promise.all(posts.map(generate301Pages))),
-  //   timeIt('About page', () => generateAboutPage()),
-  //   timeIt('Home page', () => generateHomePage(posts)),
-  //   timeIt('404 page', () => generate404Page(posts.slice(0, 5))),
-  //   timeIt('Tags pages', () => generateTagPages(groupedBlogPosts.byTag)),
-  //   timeIt('Month pages', () => generateMonthPages(groupedBlogPosts.byMonth)),
-  // ]).catch(err => {
-  //   console.error(err);
-  //   process.exit(-1);
-  // });
 
+  console.log('\nBuilding pages\n========');
   await timeIt('All posts', () => Promise.all(posts.map(generatePost)));
   await timeIt('All 301 pages', () => Promise.all(posts.map(generate301Pages)));
-  await timeIt('About page', () => generateAboutPage());
+  await timeIt('All tags pages', () => generateTagPages(groupedBlogPosts.byTag));
+  await timeIt('All month pages', () => generateMonthPages(groupedBlogPosts.byMonth));
   await timeIt('Home page', () => generateHomePage(posts));
+  await timeIt('About page', () => generateAboutPage());
   await timeIt('404 page', () => generate404Page(posts.slice(0, 5)));
-  await timeIt('Tags pages', () => generateTagPages(groupedBlogPosts.byTag));
-  await timeIt('Month pages', () => generateMonthPages(groupedBlogPosts.byMonth));
-
+  console.log('-----');
+  console.timeEnd('Overall');
+  console.log('-----');
 })();
