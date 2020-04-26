3043cc1329666ba2e4f8dfd1b67ab948f53c2ddf
wolframkriesing <wolfram+github@kriesing.de>
2020-04-26 19:33:35 +0200

    First step for a tidbits markdown parser.

---
+++ src/parse-tidbits-markdown.spec.js
@@ -0,0 +1,43 @@
+import {describe, it} from 'mocha';
+import {assertThat, containsString} from 'hamjest';
+
+import marked from 'marked';
+const defaultRenderer = new marked.Renderer();
+const tidbitsRenderer = new marked.Renderer();
+tidbitsRenderer.paragraph = (text) => {
+  if (text.startsWith('tag: ')) {
+    const tag = text.split('tag: ')[1];
+    return `<span>#${tag}</span>`;
+  }
+  return defaultRenderer.paragraph(text);
+};
+/**
+ * NOTE: we are NOT using `marked.use()` since we dont want to override other rendering, since there seems now
+ * `new Marked()` to exist.
+ * @param {string} markdown
+ * @return {string}
+ */
+const tidbitMarkdownToHtml = (markdown) => {
+  const tokens = marked.lexer(markdown);
+  return new marked.Parser({renderer: tidbitsRenderer}).parse(tokens);
+};
+
+describe('A tidbits-markdown file has an H2 followed by a tag', () => {
+  describe('GIVEN an H1 and H2 with a tag WHEN rendered', () => {
+    const tidbitMarkdown = [
+      '# H1 headline',
+      '',
+      '## A Tidbit',
+      '',
+      'tag: javascript',
+      '',
+      'tidbit content'
+    ].join('\n');
+    it('THEN render the tag like this <span>#tag</span>', () => {
+      const html = tidbitMarkdownToHtml(tidbitMarkdown);
+      const tagHtml = '<span>#javascript</span>';
+      assertThat(html, containsString(tagHtml));
+    });
+      // const tidbitHeadlineHtml = '<h2 id="a-tidbit">A Tidbit</h2>';
+  });
+});
