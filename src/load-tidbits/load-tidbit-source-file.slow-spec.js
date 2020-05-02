8fb525475c85056a0d6a91676cddeb4643f9020b
wolframkriesing <wolfram+github@kriesing.de>
2020-05-02 18:03:20 +0200

    Just making sure images are not found.

--- src/load-tidbits/load-tidbit-source-file.slow-spec.js
+++ src/load-tidbits/load-tidbit-source-file.slow-spec.js
@@ -1,5 +1,6 @@
 import {describe, it} from 'mocha';
 import assert from 'assert';
+import {assertThat, not, hasProperty, hasItem, endsWith} from 'hamjest';
 import * as path from 'path';
 import {findTidbitSourceFilenames} from '../_deps/fs.js';
 
@@ -11,12 +12,18 @@ const loadManyTidbitSourceFiles = () => async (dir) => {
 const tidbitDirectory = path.join(__dirname, '../../test-content/tidbit');
 
 describe('Load tidbit source files from a directory (tests are slow, working against a real fs)', () => {
-  it('GIVEN a tidbits-directory WHEN loading the files THEN return objects with expected `monthAndYear` and `filename`', async () => {
-    const posts = await loadManyTidbitSourceFiles()(tidbitDirectory);
-    assert(posts.length > 0);
-    assert.strictEqual(posts[0].filename, `${tidbitDirectory}/2000/01/index.md`);
-    assert.strictEqual(posts[0].monthAndYear, '2000-01');
-    assert.strictEqual(posts[1].filename, `${tidbitDirectory}/2042/12/index.md`);
-    assert.strictEqual(posts[1].monthAndYear, '2042-12');
+  describe('GIVEN a tidbits-directory WHEN loading the files THEN return objects with expected `monthAndYear` and `filename`', async () => {
+    it('THEN return objects with expected `monthAndYear` and `filename`', async () => {
+      const posts = await loadManyTidbitSourceFiles()(tidbitDirectory);
+      assert(posts.length > 0);
+      assert.strictEqual(posts[0].filename, `${tidbitDirectory}/2000/01/index.md`);
+      assert.strictEqual(posts[0].monthAndYear, '2000-01');
+      assert.strictEqual(posts[1].filename, `${tidbitDirectory}/2042/12/index.md`);
+      assert.strictEqual(posts[1].monthAndYear, '2042-12');
+    });
+    it('AND the image file does NOT show up as tidbit file', async () => {
+      const posts = await loadManyTidbitSourceFiles()(tidbitDirectory);
+      assertThat(posts, not(hasItem(hasProperty('filename', endsWith('empty-image.gif')))));
+    });
   });
 });
