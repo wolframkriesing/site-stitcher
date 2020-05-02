45bd903aca7d3c3e3be97766f8979efa32f71bdd
wolframkriesing <wolfram+github@kriesing.de>
2020-05-02 18:44:08 +0200

    Name **cough** properly. Did someone copy+paste here?

--- src/load-tidbits/load-tidbit.slow-spec.js
+++ src/load-tidbits/load-tidbit.slow-spec.js
@@ -10,7 +10,7 @@ describe('Load a tidbit file (one month)', () => {
   describe('GIVEN one source file with one tidbit in it', () => {
     it('WHEN tidbit has headline and first paragraph only THEN provide: url, dateCreated, sourceFilename, headline and abstract', async () => {
       const filename = path.join(tidbitDirectory, '2000/01/index.md');
-      const post = await loadTidbits([TidbitSourceFile.withFilename(filename)]);
+      const tidbit = await loadTidbits([TidbitSourceFile.withFilename(filename)]);
       const expectedProps = {
         url: '/tidbit/2000/a-tidbit/',
         dateCreated: '2000-01-01 10:00 CET',
@@ -18,8 +18,8 @@ describe('Load a tidbit file (one month)', () => {
         headline: 'A Tidbit',
         abstract: 'This tidbit has ONLY the required data.',
       };
-      assertThat(post[0], instanceOf(Tidbit));
-      assertThat(post[0], hasProperties(expectedProps));
+      assertThat(tidbit[0], instanceOf(Tidbit));
+      assertThat(tidbit[0], hasProperties(expectedProps));
     });
     // tags
     // main tag
