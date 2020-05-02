ef15fa98c62dc3423965dadfe625c5fc045d1c1d
wolframkriesing <wolfram+github@kriesing.de>
2020-05-02 19:07:31 +0200

    Fake more props ... ready to make it generic.

--- src/load-tidbits/load-tidbit.spec.js
+++ src/load-tidbits/load-tidbit.spec.js
@@ -1,8 +1,16 @@
 import {describe, it} from 'mocha';
 import assert from 'assert';
-import {assertThat, instanceOf} from 'hamjest';
+import {assertThat, instanceOf, hasProperties} from 'hamjest';
 
 class Tidbit {
+  constructor() {
+    this.url = '/tidbit/2111/a-tidbit/';
+    this.dateCreated = '2111-11-11 11:11 CET';
+    this.sourceFilename = '/app/test-content/tidbit/2000/01/index.md';
+    this.headline = 'A Tidbit';
+    this.abstract = 'One paragraph';
+    this.tags = [];
+  }
   static withRawData() {
     return new Tidbit();
   }
@@ -27,6 +35,21 @@ describe('Load a tidbit file (one month)', () => {
     });
     it('THEN this one be a Tidbit instance', () => {
       assertThat(load()[0], instanceOf(Tidbit));
+    });
+    it('THEN it has the headline "A Tidbit"', () => {
+      assertThat(load()[0].headline, 'A Tidbit');
+    });
+    it('THEN it has the dateCreated = 2111-11-11 11:11 CET', () => {
+      assertThat(load()[0].dateCreated, '2111-11-11 11:11 CET');
+    });
+    it('THEN it has the url = /tidbit/2111/a-tidbit/', () => {
+      assertThat(load()[0].url, '/tidbit/2111/a-tidbit/');
+    });
+    it('THEN it has the abstract = "One paragraph"', () => {
+      assertThat(load()[0].abstract, 'One paragraph');
+    });
+    it('THEN it has not tags', () => {
+      assertThat(load()[0], hasProperties({tags: []}));
     });
     // tags
     // main tag
