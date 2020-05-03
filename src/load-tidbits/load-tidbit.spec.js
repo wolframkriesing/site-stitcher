039ee118fca12153566b6b24ee22e024d19b91ce
wolframkriesing <wolfram+github@kriesing.de>
2020-05-03 14:51:15 +0200

    Split the test suites to intro the next sub case.

--- src/load-tidbits/load-tidbit.spec.js
+++ src/load-tidbits/load-tidbit.spec.js
@@ -35,38 +35,40 @@ const loadTidbitFile = (markdown) => {
 }
 
 describe('Load a tidbit file (one month)', () => {
-  describe('GIVEN one tidbit in it WHEN tidbit has all required data (headline, dateCreated, paragraph)', () => {
-    const load = () => {
-      const fileContent = [
-        '# A Tidbit', '',
-        'dateCreated: 2111-11-11 11:11 CET', '',
-        'One paragraph'
-      ].join('\n');
-      return loadTidbitFile(fileContent);
-    };
-    it('THEN find one tidbit', () => {
-      assert.equal(load().length, 1);
+  describe('GIVEN one tidbit in it', () => {
+    describe('WHEN tidbit has all required data (headline, dateCreated, paragraph)', () => {
+      const load = () => {
+        const fileContent = [
+          '# A Tidbit', '',
+          'dateCreated: 2111-11-11 11:11 CET', '',
+          'One paragraph'
+        ].join('\n');
+        return loadTidbitFile(fileContent);
+      };
+      it('THEN find one tidbit', () => {
+        assert.equal(load().length, 1);
+      });
+      it('THEN this one be a Tidbit instance', () => {
+        assertThat(load()[0], instanceOf(Tidbit));
+      });
+      it('THEN it has the headline "A Tidbit"', () => {
+        assertThat(load()[0].headline, 'A Tidbit');
+      });
+      it('THEN it has the dateCreated = 2111-11-11 11:11 CET', () => {
+        assertThat(load()[0].dateCreated, '2111-11-11 11:11 CET');
+      });
+      it('THEN it has the url = /tidbit/2111/11/a-tidbit/', () => {
+        assertThat(load()[0].url, '/tidbit/2111/11/a-tidbit/');
+      });
+      it('THEN it has the abstract = "One paragraph"', () => {
+        assertThat(load()[0].abstract, 'One paragraph');
+      });
+      it('THEN it has not tags', () => {
+        assertThat(load()[0], hasProperties({tags: []}));
+      });
+// tags
+// main tag
+// oldUrls
     });
-    it('THEN this one be a Tidbit instance', () => {
-      assertThat(load()[0], instanceOf(Tidbit));
-    });
-    it('THEN it has the headline "A Tidbit"', () => {
-      assertThat(load()[0].headline, 'A Tidbit');
-    });
-    it('THEN it has the dateCreated = 2111-11-11 11:11 CET', () => {
-      assertThat(load()[0].dateCreated, '2111-11-11 11:11 CET');
-    });
-    it('THEN it has the url = /tidbit/2111/11/a-tidbit/', () => {
-      assertThat(load()[0].url, '/tidbit/2111/11/a-tidbit/');
-    });
-    it('THEN it has the abstract = "One paragraph"', () => {
-      assertThat(load()[0].abstract, 'One paragraph');
-    });
-    it('THEN it has not tags', () => {
-      assertThat(load()[0], hasProperties({tags: []}));
-    });
-    // tags
-    // main tag
-    // oldUrls
   });
 });
