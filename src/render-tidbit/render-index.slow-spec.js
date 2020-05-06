5a0bcc0e8f658152b43d712b7e77e81f22764e1e
wolframkriesing <wolfram+github@kriesing.de>
2020-05-06 23:31:30 +0200

    Aligning text with code.

--- src/render-tidbit/render-index.slow-spec.js
+++ src/render-tidbit/render-index.slow-spec.js
@@ -1,5 +1,5 @@
 import {describe, it} from 'mocha';
-import {assertThat, equalTo, containsString, matchesPattern} from 'hamjest';
+import {assertThat, containsString, equalTo, matchesPattern} from 'hamjest';
 import {Tidbit} from "../load-tidbit/Tidbit.js";
 
 import path from 'path';
@@ -27,33 +27,25 @@ const renderTidbits = ({writeFile = prodDeps()}) => async (tidbits) => {
 
 describe('Render the tidbit index page', () => {
   describe('GIVEN one tidbit WHEN rendering the index page', () => {
-    const renderResult = tidbits => {
+    const renderResult = oneTidbit => {
       let writtenToFile = '';
       const writeFile = async (filename, content) => writtenToFile = content;
-      renderTidbits({writeFile})(tidbits);
+      renderTidbits({writeFile})([oneTidbit]);
       return writtenToFile;
     };
     it('THEN render the headline as H2', () => {
-      const tidbits = [
-        Tidbit.withRawData({headline: 'Tidbit1'}),
-      ];
-      const writtenToFile = renderResult(tidbits);
+      const writtenToFile = renderResult(Tidbit.withRawData({headline: 'Tidbit1'}));
       assertThat(writtenToFile, matchesPattern(/<h2 id="tidbit1">.*Tidbit1.*<\/h2>/gms));
     });
     it('THEN renders the SPAN for the tag', () => {
-      const tidbits = [
-        Tidbit.withRawData({headline: 'irrelevant', tags: ['a11y']}),
-      ];
-      const writtenToFile = renderResult(tidbits);
+      const writtenToFile = renderResult(Tidbit.withRawData({headline: 'irrelevant', tags: ['a11y']}));
       assertThat(writtenToFile, containsString('<span class="tag" data-tag="a11y">#a11y</span>'));
     });
     it('THEN writes the rendered string to "/tidbits/index.html"', () => {
-      const tidbits = [
-        Tidbit.withRawData({headline: 'A Tidbit', dateCreated: '2222-02-02 10:00 CET', tags: ['a11y']}),
-      ];
+      const tidbit = Tidbit.withRawData({headline: 'A Tidbit', dateCreated: '2222-02-02 10:00 CET', tags: ['a11y']});
       let writtenToFilename = '';
       const writeFile = async (filename, _) => writtenToFilename = filename;
-      renderTidbits({writeFile})(tidbits);
+      renderTidbits({writeFile})([tidbit]);
       assertThat(writtenToFilename, equalTo('/tidbits/index.html'));
     });
   });
