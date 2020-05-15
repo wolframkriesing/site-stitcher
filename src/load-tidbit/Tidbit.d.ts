b93095df8733a313ae8c6742567b89371476beb7
wolframkriesing <wolfram+github@kriesing.de>
2020-05-15 16:00:52 +0200

    Sort and reuse the metadata type, no comment needed anymore.

--- src/load-tidbit/Tidbit.d.ts
+++ src/load-tidbit/Tidbit.d.ts
@@ -10,24 +10,23 @@ type RawTidbit = {
   tags: string[];
 };
 
-export class Tidbit {
-  abstractAsHtml: string;
-  bodyAsHtml: string;
-  headline: string;
-  sourceFilename: Filename;
-  url: RelativeUrl;
-  slug: string;
-
-  dateCreated: DateTimeString;
-  tags: Tag[];
-  oldUrls: string[];
-
-  static withRawData(raw: RawTidbit): Tidbit;
-}
-
-// actually this is a duplicate of what is in `Tidbit` too mmmh
 export type TidbitMetadata = {
   dateCreated: DateTimeString;
   tags: string[];
-  oldUrls: string[];
+  oldUrls: RelativeUrl[];
+}
+
+export class Tidbit {
+  abstractAsHtml: string;
+  bodyAsHtml: string;
+  dateCreated: TidbitMetadata['dateCreated'];
+  headline: string;
+  oldUrls: TidbitMetadata['oldUrls'];
+  sourceFilename: Filename;
+  slug: string;
+  tags: TidbitMetadata['tags'];
+  url: RelativeUrl;
+
+
+  static withRawData(raw: RawTidbit): Tidbit;
 }
