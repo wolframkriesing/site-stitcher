8e9c0914de5d99698123077cceb1bbfaeaaf5e54
wolframkriesing <wolfram+github@kriesing.de>
2020-08-02 15:29:27 +0200

    Watch the templates too.

--- build-on-file-change.sh
+++ build-on-file-change.sh
@@ -2,7 +2,7 @@
 
 # See https://superuser.com/a/181543 for explanations of why the script below is used.
 
-inotifywait --event modify,create,delete --monitor --recursive src content |
+inotifywait --event modify,create,delete --monitor --recursive src content templates |
 while read -r directory event filename src; do
   echo "Watcher saw event '${event}' for file 'src/*,**/${filename}', building again...";
   npm run build
