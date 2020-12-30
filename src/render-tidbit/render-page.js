fa47920115560a243532cb5245105221c4a96b4e
wolframkriesing <wolfram+github@kriesing.de>
2020-12-30 14:22:16 +0100

    Do a different approach to inject dependencies.
    I can actually remove the function, since there is no lazy dependency resolving needed here.
    By assigning defaults to each value, I can test with injecting whatever I want,
    one function, all or none ... this is handy. Not sure if this is the
    coolest. But works quite well now. Sticking to it.

--- src/render-tidbit/render-page.js
+++ src/render-tidbit/render-page.js
@@ -1,36 +1,20 @@
 import {writeOutputFile} from '../_deps/fs.js';
-import {renderTemplate} from "../render-template.js";
+import {renderTemplate} from '../render-template.js';
 
-/**
- * @returns {import('./render-page').ProductionDependencies}
- */
-const prodDepsForIndexPage = () => {
-  return {
-    writeFile: writeOutputFile,
-    renderPage: (data) => renderTemplate('tidbit/index.html', data),
-  };
-}
-/**
- * @returns {import('./render-page').ProductionDependencies}
- */
-const prodDepsForSinglePage = () => {
-  return {
-    writeFile: writeOutputFile,
-    renderPage: (data) => renderTemplate('tidbit/page.html', data),
-  };
-}
+const renderIndexPage = (data) => renderTemplate('tidbit/index.html', data);
+const renderSinglePage = (data) => renderTemplate('tidbit/page.html', data);
 /**
  * @param deps {import('./render-page').ProductionDependencies}
  * @return {function(import('../load-tidbit/Tidbit').Tidbit[], PlainObject): Promise<void>}
  */
-export const renderAndWriteTidbitsIndexPage = ({writeFile, renderPage} = prodDepsForIndexPage()) => async (tidbits, renderParams) => {
+export const renderAndWriteTidbitsIndexPage = ({writeFile = writeOutputFile, renderPage = renderIndexPage} = {}) => async (tidbits, renderParams) => {
   await writeFile('/tidbits/index.html', renderPage({...renderParams, tidbits}));
 };
 /**
  * @param deps {import('./render-page').ProductionDependencies}
  * @return {function(import('../load-tidbit/Tidbit').Tidbit[], PlainObject): Promise<void>}
  */
-export const renderAndWriteTidbitPages = ({writeFile, renderPage} = prodDepsForSinglePage()) => async (tidbits, renderParams) => {
+export const renderAndWriteTidbitPages = ({writeFile = writeOutputFile, renderPage = renderSinglePage} = {}) => async (tidbits, renderParams) => {
   const pageWriters = tidbits.map(t => writeFile(t.url + 'index.html', renderPage({...renderParams, tidbit: t})));
   await Promise.all(pageWriters);
 };
