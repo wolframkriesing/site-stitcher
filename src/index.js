74b538341af02cf533239796c78e606c734f03a8
wolframkriesing <wolfram+github@kriesing.de>
2020-12-30 14:37:45 +0100

    Make the projects page work again.

--- src/index.js
+++ src/index.js
@@ -186,7 +186,7 @@ const loadPosts = async sourceFiles => {
   await runAndTimeIt(`All month pages (${groupedBlogPosts.byMonth.length})`, () => generateMonthPages(groupedBlogPosts.byMonth));
   await runAndTimeIt('About pages', () => generateAboutPages());
   await runAndTimeIt(`Tidbit pages (${tidbits.length})`, () => generateTidbitsPages(tidbits));
-  // await runAndTimeIt('Projects page', () => generateProjectsPage());
+  await runAndTimeIt('Projects page', () => generateProjectsPage());
   // await runAndTimeIt('Projects plan page', () => generateProjectsPlanPage());
   // await runAndTimeIt('All 301 pages', () => Promise.all(posts.map(generate301Pages)));
   // await runAndTimeIt('404 page', () => generate404Page(posts.slice(0, 5)));
