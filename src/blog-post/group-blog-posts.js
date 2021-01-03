70c8f467a60c6173dfebcb33494ba6ad11b3f0e3
wolframkriesing <wolfram+github@kriesing.de>
2021-01-03 14:31:53 +0100

    Use the slightly modified generic code, again. ;)

--- src/blog-post/group-blog-posts.js
+++ src/blog-post/group-blog-posts.js
@@ -1,4 +1,6 @@
-const postsByTag = (posts, tagSlug) => posts.filter(post => post.tags.map(t => t.slug).includes(tagSlug));
+const tagSlugsOfPost = (post) => post.tags.map(t => t.slug);
+
+const postsByTag = (posts, tagSlug) => posts.filter(post => tagSlugsOfPost(post).includes(tagSlug));
 
 const blogPostsGroupedByTag = (tagSlug, posts) => ({tagSlug, blogPosts: postsByTag(posts, tagSlug)});
 
@@ -7,36 +9,9 @@ const uniques = arr => [...new Set(arr)];
 const byBlogPostsCount = (group1, group2) => group1.blogPosts.length > group2.blogPosts.length ? -1 : 1;
 
 export const groupBlogPostsByTag = (posts) => {
-  if (posts.length === 1) {
-    const allTagsSlugs = uniques(posts.map(post => post.tags.map(t => t.slug)).flat());
-    return allTagsSlugs
-      .map(tagSlug => blogPostsGroupedByTag(tagSlug, posts))
-      .sort(byBlogPostsCount)
-    ;
-  }
-  if (posts.length === 2 && posts[0].tags.length === 2) {
-    return [
-      {tagSlug: posts[0].tags[0].slug, blogPosts: posts},
-      {tagSlug: posts[0].tags[1].slug, blogPosts: posts},
-    ]
-  }
-  if (posts.length === 2) {
-    return [
-      {tagSlug: posts[0].tags[0].slug, blogPosts: [posts[0]]},
-      {tagSlug: posts[1].tags[0].slug, blogPosts: [posts[1]]},
-    ]
-  }
-  if (posts.length === 3) {
-    return [
-      {tagSlug: posts[0].tags[2].slug, blogPosts: posts},
-      {tagSlug: posts[0].tags[1].slug, blogPosts: [posts[0], posts[1]]},
-      {tagSlug: posts[0].tags[0].slug, blogPosts: [posts[0]]},
-    ]
-  }
-
-  const allTags = uniques(posts.map(post => post.tags).flat());
-  return allTags
-    .map(tag => blogPostsGroupedByTag(tag, posts))
+  const allTagsSlugs = uniques(posts.map(tagSlugsOfPost).flat());
+  return allTagsSlugs
+    .map(tagSlug => blogPostsGroupedByTag(tagSlug, posts))
     .sort(byBlogPostsCount)
   ;
 };
