ea112ce01aa7c8f5d4316929156d31a0985cddf8
wolframkriesing <wolfram+github@kriesing.de>
2021-02-13 18:33:56 +0100

    Plan is to write a test for a macro, lets see how/if that works.

---
+++ templates/_macros.njk.test.js
@@ -0,0 +1,8 @@
+import {describe, it} from '../src/test.js';
+import {strict as assert} from 'assert';
+
+describe('macro: tagsNav - used to render a list of top and alphabetically sorted tags', () => {
+  it('ccc', () => {
+    
+  });
+});
